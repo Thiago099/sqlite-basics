111dd3076ab0e5a76c71e48f6420f1033168fed0
Thiago099 <tecnologiasthiago@gmail.com>
2022-07-02 19:24:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+});
+db.exec(`
+    CREATE TABLE entity
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(64)
+    );
+    `)
+db.exec(`
+    INSERT INTO entity (name) VALUES ('John');
+    INSERT INTO entity (name) VALUES ('Jane');
+    INSERT INTO entity (name) VALUES ('Bob');
+`)
+db.all('SELECT * FROM entity',(err,rows)=>{
+    console.log(rows);
+})
