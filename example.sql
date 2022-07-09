3b4cad47c3998c3daa40868ff67e0e1e468aa37c
Thiago099 <thiago.kaique01@gmail.com>
2022-07-09 19:10:27 -0300

    [fix]

--- example.sql
+++ example.sql
@@ -10,11 +10,11 @@ CREATE TABLE adjective
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64)
 );
-INSERT INTO adjective (name) VALUES ('handsome'), ('ugly'), ('fat'), ('thin'), ('tall'), ('short');
+INSERT INTO adjective (name) VALUES ('handsome'), ('ugly'), ('fat'), ('thin'), ('tall'), ('short'), ('hot'), ('cold'),('burnt');
 
 CREATE TABLE verb
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64)
 );
-INSERT INTO verb (name) VALUES ('jump'), ('swim'), ('walk'), ('run'), ('play'), ('eat');
+INSERT INTO verb (name) VALUES ('jump'), ('swim'), ('walk'), ('run'), ('play'), ('eat'), ('touch'), ('drink'), ('sleep'),('burn');
