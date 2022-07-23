9d578805e8a9903e5b19f612baf1437f2cc04949
Thiago099 <tecnologiasthiago@gmail.com>
2022-07-23 11:04:46 -0300

    [refactor]

--- example.sql
+++ example.sql
@@ -3,7 +3,7 @@ CREATE TABLE entity
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64)
 );
-INSERT INTO entity VALUES (1, 'eletronic'), (2, 'hands'), (3, 'something'), (4, 'something hot'), (5, 'burned hands');
+INSERT INTO entity VALUES (1, 'eletronic'), (2, 'hands'), (3, 'something');
 
 CREATE TABLE state
 (
@@ -18,47 +18,3 @@ CREATE TABLE action
     name VARCHAR(64)
 );
 INSERT INTO action VALUES (1, 'wet'), (2, 'touch'), (3, 'burn'), (4, 'break');
-
-CREATE TABLE instance
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entity_from_id INTEGER,
-    entity_to_id INTEGER,
-    FOREIGN KEY(entity_from_id) REFERENCES entity(id),
-    FOREIGN KEY(entity_to_id) REFERENCES entity(id)
-);
-
-INSERT INTO instance VALUES (1, 3, 4), (2, 3,2), (3, 3, 1),(4, 2, 5);
-
-CREATE TABLE entity_state
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entity_id INTEGER,
-    state_id INTEGER,
-    FOREIGN KEY(entity_id) REFERENCES entity(id),
-    FOREIGN KEY(state_id) REFERENCES state(id)
-);
-
-INSERT INTO entity_state VALUES (1, 4, 3), (2, 5, 2);
-
-CREATE TABLE entity_action
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entity_id INTEGER,
-    action_id INTEGER,
-    FOREIGN KEY(entity_id) REFERENCES entity(id),
-    FOREIGN KEY(action_id) REFERENCES action(id)
-);
-
-INSERT INTO entity_action VALUES (1, 4, 2), (2, 2, 3), (3, 1, 1), (4, 1, 4);
-
-CREATE TABLE entity_action_causality
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entity_action_cause_id INTEGER,
-    entity_action_effect_id INTEGER,
-    FOREIGN KEY(entity_action_cause_id) REFERENCES entity_action(id),
-    FOREIGN KEY(entity_action_effect_id) REFERENCES entity_action(id)
-);
-
-INSERT INTO entity_action_causality VALUES (1, 1, 2), (2, 2, 3);
--- index.js
+++ index.js
@@ -3,35 +3,10 @@ var fs = require('fs');
 db.serialize(() => {
     db.run(fs.readFileSync('./example.sql').toString())
     db.query(`
-        SELECT entity_from.name entity_from, entity_to.name entity_to
-        FROM instance
-        INNER JOIN entity entity_from ON entity_from.id = instance.entity_from_id
-        INNER JOIN entity entity_to ON entity_to.id = instance.entity_to_id
+        SELECT * from entity
         `,result => {
         console.table(result);
     })
 
-    db.query(`
-        SELECT entity.name entiy, state.name state
-        FROM entity_state
-        INNER JOIN entity ON entity.id = entity_state.entity_id
-        INNER JOIN state ON state.id = entity_state.state_id
-        `,result => {
-        console.table(result);
-    })
-    db.query(`
-        SELECT entity.name entity, action.name action
-        FROM entity_action
-        INNER JOIN entity ON entity.id = entity_action.entity_id
-        INNER JOIN action ON action.id = entity_action.action_id
-        `,result => {
-        console.table(result);
-    })
-    db.query(`
-        SELECT entity_action_cause_id cause, entity_action_effect_id effect
-        FROM entity_action_causality
-        `,result => {
-        console.table(result);
-    })
     db.close();
 })
